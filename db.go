94fb91dbad348b9c23d3e58a45ef4350385d58f0
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2017-10-06 06:13:11 +0200

    Add other db engines

--- db.go
+++ db.go
@@ -4,16 +4,16 @@ import (
 	"fmt"
 	"net/url"
 
-	// _ "github.com/denisenkom/go-mssqldb"
-	// _ "github.com/go-sql-driver/mysql"
-	// _ "github.com/lib/pq"
+	_ "github.com/denisenkom/go-mssqldb"
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
 
 	"github.com/jakubknejzlik/go-survey/model"
 	"github.com/jinzhu/gorm"
-	// _ "github.com/jinzhu/gorm/dialects/mssql"
-	// _ "github.com/jinzhu/gorm/dialects/mysql"
-	// _ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/jinzhu/gorm/dialects/mssql"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
